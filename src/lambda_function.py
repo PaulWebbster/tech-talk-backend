c40ca8fc1d11845e52c9d27467af26d8becd1b3a
kqcg440_azu <kqcg440@astarazeneca.net>
2025-05-08 12:08:49 +0200

    Stage 6: Add GET endpoint for getting conferences

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,12 +1,24 @@
 import json
 import boto3
+import logging
 from boto3.dynamodb.conditions import Key
+
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('conferences')
 
 def lambda_handler(event, context):
-    if event['httpMethod'] == 'GET' and event['path'] == '/conferences':
+    # Log the incoming event
+    logger.info("Received event: %s", json.dumps(event))
+    
+    # Extract HTTP method and path from the event
+    http_method = event['requestContext']['http']['method']
+    path = event['rawPath']
+    
+    if http_method == 'GET' and path == '/conferences':
         response = table.scan()
         return {
             'statusCode': 200,
